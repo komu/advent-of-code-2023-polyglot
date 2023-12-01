4398315667019f3961d4e9cd5c6fed83854cef20
Juha Komulainen <juha.komulainen@evident.fi>
2023-12-01 17:11:30 +0200

    Make script reproducible using Nix

--- src/day1/run.sh
+++ src/day1/run.sh
@@ -1,3 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash --pure
+#! nix-shell -p nodejs
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/293822e55ec1.tar.gz
 
+npm install
 npx tsc day1.ts
